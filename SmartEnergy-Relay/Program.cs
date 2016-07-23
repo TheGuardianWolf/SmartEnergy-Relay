7c23b50d7581e9396dfa3f77a0a597444ad31aac
Aeon Firebrand <lichking21st@windowslive.com>
2016-07-24 10:57:36 +1200

    Initial Commit
    Started new console project

---
+++ SmartEnergy-Relay/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SmartEnergy_Relay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(new TimeSpan(1, 0, 0));
+        }
+    }
+}
